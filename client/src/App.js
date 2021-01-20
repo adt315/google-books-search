ebfc2b192d546888e6ee18b8a8dee67c88e97f6f
adt315 <angela_dyan@hotmail.com>
2021-01-20 14:13:09 -0500

    added components

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,24 @@
 import React from "react";
 // import ReactDOM from "react-dom";
+import Navbar from "./components/Navbar";
+import Wrapper from "./components/Wrapper";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 
 function App() {
   return (
-    <div className="App">
-        <p>
-          Hello World!
-        </p>
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+        <Wrapper>
+          {/* <Route exact path="/" component={About} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/portfolio" component={Portfolio} />
+          <Route exact path="/contact" component={Contact} />
+          <Route exact path="/resume" component={Resume} /> */}
+        </Wrapper>
+      </div>
+    </Router>
   );
 }
 
